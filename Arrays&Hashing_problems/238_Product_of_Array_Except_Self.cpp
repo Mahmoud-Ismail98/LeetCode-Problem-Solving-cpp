f82fc73ed28ab793094ef64cd8a506ec65b3bd86
Mahmoud-Koraim <m.koraim77@gmail.com>
2024-04-18 12:30:43 +0200

    238. Product of Array Except Self

---
+++ Arrays&Hashing_problems/238_Product_of_Array_Except_Self.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int total_sum = 1;
+        vector<int> result(nums.size(), 0); // Initialize result vector with zeros
+        int zero_flag = -1;
+        int zero_count = 0;
+        
+        // First pass to calculate total product and count zeros
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == 0) {
+                zero_flag = i;
+                zero_count++;
+                if (zero_count > 1) break; // If more than one zero, no need to continue
+                continue;
+            }
+            total_sum *= nums[i];
+        }
+        
+        if (zero_count > 1) return result; // If more than one zero, return all zeros
+        
+        // Second pass to calculate product except itself
+        for (int i = 0; i < nums.size(); ++i) {
+            if (zero_count == 1) {
+                if (i == zero_flag) result[i] = total_sum; // If one zero, set the product except itself
+            } else {
+                result[i] = total_sum / nums[i]; // Calculate product except itself
+            }
+        }
+        
+        return result;
+    }
+};
+
+int main() {
+    // Test cases
+    vector<vector<int>> testCases = {
+        {1, 2, 3, 4},    // Output: [24,12,8,6]
+        {-1, 1, 0, -3, 3} // Output: [0,0,9,0,0]
+    };
+    
+    Solution solution;
+    for (auto& nums : testCases) {
+        vector<int> result = solution.productExceptSelf(nums);
+        cout << "Output: [";
+        for (int i = 0; i < result.size(); ++i) {
+            cout << result[i];
+            if (i < result.size() - 1) cout << ",";
+        }
+        cout << "]" << endl;
+    }
+    
+    return 0;
+}
