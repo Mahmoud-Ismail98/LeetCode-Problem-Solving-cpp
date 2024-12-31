1146950eac6cdfc2c72be02506fc48aa87697080
Mahmoud-Ismail-Korayem <63348980+Mahmoud-Ismail98@users.noreply.github.com>
2024-12-31 22:01:00 +0200

    optimize Time Complexity to be o(n) and space complexity to be o(1) int the second solution

--- Arrays&Hashing_problems/238_Product_of_Array_Except_Self.cpp
+++ Arrays&Hashing_problems/238_Product_of_Array_Except_Self.cpp
@@ -3,40 +3,59 @@
 
 using namespace std;
 
-class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        int total_sum = 1;
-        vector<int> result(nums.size(), 0); // Initialize result vector with zeros
-        int zero_flag = -1;
-        int zero_count = 0;
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int total_sum = 1;
+//         vector<int> result(nums.size(), 0);
+//         int zero_flag = -1;
+//         int zero_count = 0;       
+//         // First pass to calculate total product and count zeros
+//         for (int i = 0; i < nums.size(); ++i) {
+//             if (nums[i] == 0) {
+//                 zero_flag = i;
+//                 zero_count++;
+//                 if (zero_count > 1) break; // If more than one zero, no need to continue
+//                 continue;
+//             }
+//             total_sum *= nums[i];
+//         }
         
-        // First pass to calculate total product and count zeros
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == 0) {
-                zero_flag = i;
-                zero_count++;
-                if (zero_count > 1) break; // If more than one zero, no need to continue
-                continue;
-            }
-            total_sum *= nums[i];
+//         if (zero_count > 1) return result; // If more than one zero, return all zeros
+        
+//         // Second pass to calculate product except itself
+//         for (int i = 0; i < nums.size(); ++i) {
+//             if (zero_count == 1) {
+//                 if (i == zero_flag) result[i] = total_sum; // If one zero, set the product except itself
+//             } else {
+//                 result[i] = total_sum / nums[i]; // Calculate product except itself
+//             }
+//         }
+        
+//         return result;
+//     }
+// };
+// another solution
+class Solution{
+    public:
+    vector<int> productExceptSelf(vector<int>&nums){
+        vector<int>result(nums.size(),1);
+        int prefix=1;
+        for (int i = 0; i <nums.size() ; i++)
+        {   
+            result[i]=prefix;
+            prefix*=nums[i];
+        }
+        int postfix=1;
+        for (int i = nums.size()-1; i >0 ; i--)
+        {
+            result[i]*=postfix;
+            postfix*=nums[i];
         }
         
-        if (zero_count > 1) return result; // If more than one zero, return all zeros
-        
-        // Second pass to calculate product except itself
-        for (int i = 0; i < nums.size(); ++i) {
-            if (zero_count == 1) {
-                if (i == zero_flag) result[i] = total_sum; // If one zero, set the product except itself
-            } else {
-                result[i] = total_sum / nums[i]; // Calculate product except itself
-            }
-        }
-        
-        return result;
+     return result;   
     }
 };
-
 int main() {
     // Test cases
     vector<vector<int>> testCases = {
