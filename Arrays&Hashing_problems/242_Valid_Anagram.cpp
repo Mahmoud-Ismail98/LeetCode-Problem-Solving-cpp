cc2d478fdd01b3019fcf80baa71b8e28b83e550d
Mahmoud-Ismail-Korayem <63348980+Mahmoud-Ismail98@users.noreply.github.com>
2024-12-31 23:34:50 +0200

    another solution for valid anagram

--- Arrays&Hashing_problems/242_Valid_Anagram.cpp
+++ Arrays&Hashing_problems/242_Valid_Anagram.cpp
@@ -8,20 +8,42 @@ public:
         if (s.size() != t.size()) {
             return false;
         }
-
         // Count the frequency of each character in s
         unordered_map<char, int> freq;
         for (char ch : s) {
             freq[ch]++;
         }
-
         // Decrease the frequency of each character in t
         for (char ch : t) {
             if (--freq[ch] < 0) {
                 return false; // If the frequency becomes negative, it means t contains a character not present in s
             }
         }
-
+        return true;
+    }
+    bool isAnagram2(string s,string t)
+    {
+        vector<int>result(26,0);
+        if(s.size()!=t.size())
+        {
+            return false;
+        }
+        for(char ch :s)
+        {
+            result[ch-'a']++;
+        }
+        for(char ch :t)
+        {
+            result[ch-'a']--;
+        }
+        for (auto it:result)
+        {
+           if(it!=0) 
+           {
+             return false;
+           }
+        }
+        
         return true;
     }
 };
@@ -35,10 +57,10 @@ int main() {
     string t2 = "car";
 
     Solution solution;
-    bool result1 = solution.isAnagram(s1, t1);
+    bool result1 = solution.isAnagram2(s1, t1);
     cout << "Result 1: " << (result1 ? "true" : "false") << endl;
 
-    bool result2 = solution.isAnagram(s2, t2);
+    bool result2 = solution.isAnagram2(s2, t2);
     cout << "Result 2: " << (result2 ? "true" : "false") << endl;
 
     return 0;
